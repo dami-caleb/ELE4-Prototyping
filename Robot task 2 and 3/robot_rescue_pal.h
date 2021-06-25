003f45c1dfefc0e2b130d84b32b0cdc0af8a6208
johnsondarko3 <72194602+johnsondarko3@users.noreply.github.com>
2021-06-25 08:42:38 +0200

    Create robot_rescue_pal.h

---
+++ Robot task 2 and 3/robot_rescue_pal.h
@@ -0,0 +1,10 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+static int driving_mode = 0;
+static char coming_from = 'O';
+static int status = 0;
+// Function declaration
+int move(char *world);
+
+#endif // ROBOT_H
